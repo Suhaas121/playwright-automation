ef6dbcdac16f7b480ef5f5cff44ca194403da487
SUHAAS PATEL AV <suhaaspatelav@Mac.lan>
2025-11-29 09:24:06 +0530

    yaml fix

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 import * as dotenv from 'dotenv';
+dotenv.config();
 import path from 'path';
 
 // ---------------------------
